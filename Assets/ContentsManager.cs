de548e891d88126248383d217e36067e3e1e5bde
mwbullen <mwbullen@gmail.com>
2017-07-13 14:52:10 -0400

    adding content management

---
+++ Assets/ContentsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class ContentsManager : MonoBehaviour {
+	ContentsManagerInfo contentInfo;
+
+	// Use this for initialization
+	void Start () {
+		contentInfo = loadContentInfo ();
+	}
+
+	void displayContents() {
+		foreach (int i in contentInfo.contentHT.Keys) {
+			GameObject child = gameObject.transform.GetChild (i).gameObject;
+			if (i != null) {
+
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void saveContentInfo() {
+		//serialize contentinfo
+	}
+
+	ContentsManagerInfo loadContentInfo() {
+		//load contentinfo from disk or return new
+
+		return null;
+	}
+}
---
+++ Assets/ContentsManagerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class ContentsManagerInfo
+	{
+		public System.Collections.Hashtable contentHT;
+
+		public ContentsManagerInfo ()
+		{
+		}
+	}
+}
+
