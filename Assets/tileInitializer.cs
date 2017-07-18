139ef1e78e10a346a54f4fcc8067d36ae0b30448
mwbullen <mwbullen@gmail.com>
2017-07-18 13:13:00 -0400

    mouse drag logic as child prefab attached at startup

---
+++ Assets/tileInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tileInitializer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControl = GameObject.Find ("GameControl");
+
+		GameObject newTileDragFSMChild = GameObject.Instantiate( gameControl.GetComponent<prefabManager> ().tileDragControlPrefab);
+
+		newTileDragFSMChild.transform.SetParent (gameObject.transform);
+		newTileDragFSMChild.transform.localPosition = new Vector3 (0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
